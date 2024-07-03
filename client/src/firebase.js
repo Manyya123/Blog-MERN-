de933dc425704297c070ef6037e0ca1e8c4add93
Manyya123 <manyaagrawal36@gmail>
2024-07-04 00:09:22 +0530

    Update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -1,17 +1,16 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+  authDomain: "blogmern-c3693.firebaseapp.com",
+  projectId: "blogmern-c3693",
+  storageBucket: "blogmern-c3693.appspot.com",
+  messagingSenderId: "562755785234",
+  appId: "1:562755785234:web:f3aaddb32d57d58ad43ecb"
 };
 
 // Initialize Firebase
