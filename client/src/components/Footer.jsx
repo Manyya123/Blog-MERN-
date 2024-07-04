85f98aa32bd201c5baeead1fdcbb24d72ad71f2c
Manyya123 <manyaagrawal36@gmail>
2024-07-04 16:26:32 +0530

    update

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -12,7 +12,7 @@ export default function FooterCom() {
               className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
             >
               <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
-                Sahand's
+                Manya's
               </span>
               Blog
             </Link>
@@ -22,18 +22,11 @@ export default function FooterCom() {
               <Footer.Title title='About' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.100jsprojects.com'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                >
-                  100 JS Projects
-                </Footer.Link>
-                <Footer.Link
                   href='/about'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
-                  Sahand's Blog
+                  Manya's Blog
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
@@ -41,7 +34,7 @@ export default function FooterCom() {
               <Footer.Title title='Follow us' />
               <Footer.LinkGroup col>
                 <Footer.Link
-                  href='https://www.github.com/sahandghavidel'
+                  href='https://www.github.com/Manyya123'
                   target='_blank'
                   rel='noopener noreferrer'
                 >
@@ -63,7 +56,7 @@ export default function FooterCom() {
         <div className='w-full sm:flex sm:items-center sm:justify-between'>
           <Footer.Copyright
             href='#'
-            by="Sahand's blog"
+            by="Manya's blog"
             year={new Date().getFullYear()}
           />
           <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
